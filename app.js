73ece420d2de2a1b16011c766630c180c51fe8df
Keshab Sarkar <152651492+Keshab1997@users.noreply.github.com>
2025-09-02 12:31:55 +0000

    update

--- app.js
+++ app.js
@@ -89,7 +89,9 @@ document.addEventListener("DOMContentLoaded", () => {
             } else {
                 alert("এই পরীক্ষা দিতে হলে আপনাকে লগইন করতে হবে!");
                 window.location.href =
-                    "https://keshab1997.github.io/Study-With-Keshab/login.html";
+                    "https://keshab1997.github.io/Study-With-Keshab/login.html?redirect=exams/index.html?exam=" +";
+
+
             }
         });
     } else {
